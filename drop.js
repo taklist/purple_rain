b305314a24805b19a94cc9c43a692d03588d0bc6
taklist <bjorn.janson.94@gmail.com>
2017-07-04 13:19:55 +0200

    Now got a "flat" umbrella

--- drop.js
+++ drop.js
@@ -1,20 +1,34 @@
 function Drop () {
     this.x = random(width);
     this.y = random(-500, -50);
-    this.len = 20;
-    this.speed = 5;
+    this.len = random(10, 20);
+    this.speed = random(4, 10);
 
     this.fall = function() {
         this.y = this.y + this.speed;
 
-        if (this.y > height) {
-            this.y = -20;
+        if (this.y > height || hitUmbrella(this.x, this.y, this.len, this.speed)) {
+            this.x = random(width);
+            this.y = random(-500, -50);
+            this.len = random(10, 20);
+            this.speed = random(4, 10);
         }
     }
 
     this.show = function() {
         strokeWeight(2);
         stroke(138, 43, 226);
-        line(this.x, this.y, this.x, this.y + this.len);
+        line(this.x, this.y, this.x, this.y - this.len);
+    }
+}
+
+function hitUmbrella(x, y, len, speed) {
+    if (mouseIsPressed) {
+       if (x < mouseX + 50 && x > mouseX - 50) {
+            if (y - speed < mouseY && y > mouseY) {
+                console.log("HIT!");
+                return true;
+            }
+       }
     }
 }
--- sketch.js
+++ sketch.js
@@ -11,6 +11,12 @@ function setup() {
 
 function draw() { 
     background(230,230,250);
+
+    if (mouseIsPressed) {
+        stroke(0, 0, 0);
+        line(mouseX - 50, mouseY, mouseX + 50, mouseY);
+    }
+
     for (var i = 0; i < drops.length; i++) {
         drops[i].fall(); 
         drops[i].show();
